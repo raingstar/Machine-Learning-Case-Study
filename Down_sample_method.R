e466cebbd38bd0f1b0e16794264539ff31c92238
Keyu <knie@rain-phx02-1461511.phx02.dev.ebayc3.com>
2017-05-26 17:38:47 -0700

    fix bug in down sample

--- Down_sample_method.R
+++ Down_sample_method.R
@@ -7,9 +7,10 @@ for i
 train <- fread("~/training_new.csv", header=T)    ###Read training set. Fread is good for now.
 
 ########down sample negative part, choose only buck_id==group_id or positive sample
+train <- train %>%
+       mutate(buck_id=sample(seq(1,10),dim(train)[1],replace = T))
 for (group_id in 1:10){ ###global variable, set it from 1 to 10
 train_save<- train %>% 
-       mutate(buck_id=sample(seq(1,10),dim(train)[1],replace = T)) %>%
        filter((buck_id==group_id & orderlabel==0) | ( orderlabel==1) ) %>%
        mutate(buck_id=NULL)
 fwrite(train_save, paste("training_new_sub",group_id,sep = "_") )
